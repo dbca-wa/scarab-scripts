10d9c0b3096a2b42b832cdaff990356a8228a322
Milly Piggott <Camilla.Piggott@dbca.wa.gov.au>
2020-03-19 01:57:53 +0000

    install wastdr to specified lib

--- helpers.R
+++ helpers.R
@@ -21,7 +21,7 @@
 # remotes::install_github("r-lib/lifecycle")
 # remotes::install_github("ropensci/rredlist")
 # remotes::install_github("dbca-wa/wastdr")
-
+# remotes::install_github("dbca-wa/wastdr",lib = "/home/milly/R/library" )
 
 library(tidyverse)
 
